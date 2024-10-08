e81978141ad5d5d33a9aa804392e29571259e643
Jose Rafael Verduzco-Torres <JoseRafael.Verduzco-Torres@glasgow.ac.uk>
2024-10-08 12:09:49 +0100

    Update access function to work with new column names, e.g. origin to from_id.

--- R/00_accessibility_function.R
+++ R/00_accessibility_function.R
@@ -68,7 +68,7 @@ accessibility <-
     # Check if it includes mu parameter
     if (mu != 0 & fn != "cum") {
       # Compute delta
-      ttm[, delta := fifelse(origin == destination, 1, 0)]
+      ttm[, delta := fifelse(from_id == to_id, 1, 0)]
       # Compute access weights for each OD pair considering mu and delta
       ttm[, access_w := dec_fn(d_ij = travel_time, w = dest_w, gamma = gamma, beta = beta, mu = mu, delta_ij = delta)]
     } else {
@@ -80,11 +80,11 @@ accessibility <-
     if (opportunity == "all") {
       # Summarise access by origin
       access_dt <- ttm[, list(accessibility = sum(access_w, na.rm = T)),
-        by = .(origin)
+        by = .(from_id)
       ]
     } else if (opportunity == "k") {
       # Make available opportunities if i = j
-      ttm[, dest_inc_decile := fifelse(origin == destination, or_edu_decile, dest_inc_decile)]
+      ttm[, dest_inc_decile := fifelse(from_id == to_id, or_edu_decile, dest_inc_decile)]
       # Summarise by origin and k
       # If type of origin is NA, then access is NA
       access_dt <- ttm[!is.na(or_edu_decile),
@@ -92,12 +92,12 @@ accessibility <-
           accessibility = sum(access_w, na.rm = T),
           or_dec = first(or_edu_decile)
         ),
-        by = .(origin, dest_inc_decile)
+        by = .(from_id, dest_inc_decile)
       ]
       # Subset access weights that match k in O and D and summarise by origin
       access_dt <- access_dt[or_dec == dest_inc_decile,
         .(accessibility = sum(accessibility, na.rm = T)),
-        by = .(origin)
+        by = .(from_id)
       ]
     } else {
       stop("Unkown opportunity type")
