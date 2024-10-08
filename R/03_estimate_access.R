6f8ed38215722f8c8f7ac5715d8dc599b6df8044
Jose Rafael Verduzco-Torres <JoseRafael.Verduzco-Torres@glasgow.ac.uk>
2024-10-08 12:11:29 +0100

    Changes to recalculate accessibility using updated ttm calculated in R instead of those used in PhD.

--- R/03_estimate_access.R
+++ R/03_estimate_access.R
@@ -33,10 +33,10 @@ source("R/00_accessibility_function.R")
 grids <- c("grid_1000", "grid_2000", "grid_4000", "grid_500",  "postcodes")
 grids <- setNames(grids, grids)
 
-ttm_base_path <- "data/ttm_adjusted/"
+ttm_base_path <- "data/ttm_mexicocity//"
 
 # create directory to store temp results
-out <- "data/accessibility/"
+out <- "data/accessibility_r5r/"
 dir.create(out)
 
 # Read data at origin and destination -------------------------------------
@@ -47,6 +47,7 @@ data_destination <-
   map(select, -est_remun_tot:-oc_pe_den_sq_km) %>%
   map(rename_at, vars(-id), ~ paste0("dest_", .)) %>%
   setNames(grids)
+data_destination <- rbindlist(data_destination, idcol = 'grid')
 
 # Read population census data (origin)
 data_origin <-
@@ -54,12 +55,15 @@ data_origin <-
   map(select, id, edu49_r, edu_decile) %>%
   map(rename_at, vars(-id), ~ paste0("or_", .)) %>%
   setNames(grids)
+data_origin <- rbindlist(data_origin, idcol = 'grid')
 
 # Recalculate matching ntiles between education and employment salary?
 ntiles <- 5
-map(data_destination, ~ .x[, dest_inc_decile := ntile(dest_avg_income, ntiles)])
-map(data_origin, ~ .x[, or_edu_decile := ntile(or_edu49_r, ntiles)])
+data_destination[, dest_inc_decile := ntile(dest_avg_income, ntiles), by = "grid"]
+data_origin[, or_edu_decile := ntile(or_edu49_r, ntiles), by = "grid"]
 
+# Within-zone travel time
+intra_ttm <- data.table::fread('data/ttm_mexicocity/intra_tt.csv')
 
 
 # Parameters --------------------------------------------------------------
@@ -72,159 +76,174 @@ acc_pars <- tribble(
   "exp", 0.044, 1.654, "k", "pt",
   "exp", 0.031, 1.662, "all", "pt",
   "exp", 0.031, 1.662, "k", "pt",
-  "exp", 0.085, 0, "all", "car"
+  "exp", 0.085, 1.654, "all", "car",
+  "exp", 0.085, 1.654, "k", "car"
 )
 write_csv(acc_pars, "data/accessibility/access_pars.csv")
 
 # Public transport --------------------------------------------------------
 
+# Find files for PT TTM
+ttm_paths_pt <- list.files('data/ttm_mexicocity/ttm_pt/', full.names = TRUE)
 
-# Public transport routers
-routers <- c(
-  "before_2010",
-  "2010_2012",
-  "2012_2013",
-  "2013_2015",
-  "2015_2016",
-  "2016_2018",
-  "2018_2019"
-)
 
-# TTM paths
-ttm_paths_pt <- 
-  map(
-    paste0(ttm_base_path, grids),
-    list.files,
-    full.names = TRUE, 
-    pattern = "pt"
-  ) %>%
-  setNames(grids)
-
-grid_n <- 5
-router_n <- 1
-
-# Run loop
-# Loop level 1- Grids 1-5
-for (grid_n in 1:length(grids)) {
+# Run loop for each TTM
+for (ttm_n in ttm_paths_pt) {
   
-  # Index
-  print(paste("Processing grid", grid_n, "of", length(grids)))
-  # Initialize list for current grid
-  access_by_grid <- list()
+  # Read TTM for grid_n and router_n
+  ttm <- data.table::fread(ttm_n)
   
-  # Loop level 2 - Routers in current grid, 1-7
-  for (router_n in 1:length(routers)) {
-    # Router index
-    print(paste("Router", router_n, "of", length(routers)))
-
-    # Read TTM for grid_n and router_n
-    ttm <- readRDS(ttm_paths_pt[[grid_n]][router_n])
-    # Join data to ttm
-    ttm[data_origin[[grid_n]], on = c(origin = "id"), or_edu_decile := or_edu_decile]
-    ttm[data_destination[[grid_n]],
-      on = c(destination = "id"),
-      c("dest_est_ec_un", "dest_est_pe_oc", "dest_inc_decile") :=
-        list(dest_est_ec_un, dest_est_pe_oc, dest_inc_decile)
-    ]
-
-    # Apply access function for different parameters
-    access_all_par <- lapply(1:sum(acc_pars$mode == "pt"), function(i) {
-      # Estimate accessibility
-      access_origin <-
-        accessibility(ttm,
-          w = "dest_est_pe_oc",
-          fn = acc_pars$dec_fn[i],
-          beta = acc_pars$beta[i],
-          mu = acc_pars$mu[i],
-          opportunity = acc_pars$opportunity[i]
-        )
-      # Assign parameters used for estimates
-      access_origin[, dec_fn := acc_pars$dec_fn[i]]
-      access_origin[, beta := acc_pars$beta[i]]
-      access_origin[, mu := acc_pars$mu[i]]
-      access_origin[, opportunity := acc_pars$opportunity[i]]
-      # Return result
-      return(access_origin)
-    })
-
-    lapply(access_all_par, summary)
-
-
-    # Bind rows
-    access_all_par <- rbindlist(access_all_par)
-    # Assign router
-    access_all_par[, router := routers[router_n]]
-    # Store results in list
-    access_by_grid[[routers[router_n]]] <- access_all_par
-
-    # Clean env.
-    rm(ttm, access_all_par)
-    gc()
-  }
-
-  # Bind grid results
-  access_by_grid <- rbindlist(access_by_grid)
-  # Save result by grid
-  write_csv(
-    access_by_grid, 
-    paste0(out, "accessibility_", grids[grid_n], "_pt.csv")
-  )
-  
-  # Clean env.
-  rm(access_by_grid)
-  gc()
-}
-
-# Accessibility by car ----------------------------------------------------
-
-# TTM paths by car
-ttm_paths_car <-
-  list.files(ttm_base_path, recursive = TRUE, pattern = "car", full.names = TRUE)
-
-# Run loop for each grid
-for (grid_n in 1:length(grids)) {
-  # Index
-  print(paste("Processing grid", grid_n, "of", length(grids)))
-
-  # Read TTM for grid_n
-  ttm <- readRDS(ttm_paths_car[grid_n])
   # Join data to ttm
-  ttm[data_origin[[grid_n]], on = c(origin = "id"), or_edu_decile := or_edu_decile]
-  ttm[data_destination[[grid_n]],
-    on = c(destination = "id"),
-    c("dest_est_ec_un", "dest_est_pe_oc", "dest_inc_decile") :=
-      list(dest_est_ec_un, dest_est_pe_oc, dest_inc_decile)
+  # Education data
+  ttm[
+    data_origin, 
+    on = c(grid = "grid", from_id = "id"), 
+    or_edu_decile := or_edu_decile
   ]
-
+  # Economic census data
+  ttm[
+    data_destination, 
+    on = c(grid = "grid", to_id = "id"),
+    c("dest_est_ec_un", "dest_est_pe_oc", "dest_inc_decile") :=
+        list(dest_est_ec_un, dest_est_pe_oc, dest_inc_decile)
+  ]
+  # Within-zone data
+  ttm[
+    intra_ttm, 
+    on = c(grid = "grid", from_id = "id"),
+    intra_tt_walk := intra_tt_walk
+  ]
+  ttm[, travel_time := fifelse(
+    from_id == to_id, 
+    intra_tt_walk, 
+    travel_time_p50)
+  ]
+  
+  
   # Apply access function for different parameters
-  access_all_par <- lapply(6, function(i) {
+  access_all_par <- lapply(1:sum(acc_pars$mode == "pt"), function(i) {
+    
     # Estimate accessibility
-    access_origin <-
-      accessibility(ttm,
-        w = "dest_est_pe_oc",
-        fn = acc_pars$dec_fn[i],
-        beta = acc_pars$beta[i],
-        mu = acc_pars$mu[i],
-        opportunity = acc_pars$opportunity[i]
-      )
+    access_origin <- accessibility(
+      ttm = ttm,
+      w = "dest_est_pe_oc",
+      fn = acc_pars$dec_fn[i],
+      beta = acc_pars$beta[i],
+      mu = acc_pars$mu[i],
+      opportunity = acc_pars$opportunity[i]
+    )
+    
     # Assign parameters used for estimates
     access_origin[, dec_fn := acc_pars$dec_fn[i]]
     access_origin[, beta := acc_pars$beta[i]]
     access_origin[, mu := acc_pars$mu[i]]
     access_origin[, opportunity := acc_pars$opportunity[i]]
+    
     # Return result
     return(access_origin)
   })
-
+  
+  
   # Bind rows
   access_all_par <- rbindlist(access_all_par)
+  # Assign router
+  access_all_par[, router := unique(ttm$router)]
+  # Assign grid
+  access_all_par[, grid := unique(ttm$grid)]
+  
   # Save result by grid
-  write_csv(access_all_par, paste0(out, "accessibility_", grids[grid_n], "_car.csv"))
-
+  write_csv(
+    access_all_par, 
+    paste0(out, "accessibility_", unique(ttm$grid), "_", unique(ttm$router), "_pt.csv")
+  )
+  
   # Clean env.
   rm(ttm, access_all_par)
   gc()
 }
+  
+  
+
+# Accessibility by car ----------------------------------------------------
+
+# TTM paths by car
+ttm_paths_car <-
+  list.files('data/ttm_adjusted', recursive = TRUE, pattern = "car", full.names = TRUE)
+
+
+# Run loop for each TTM
+for (ttm_n in ttm_paths_car) {
+  # Set grid
+  current_grid <- strsplit(ttm_n, "/")[[1]][3]
+  
+  # Read TTM for grid_n and router_n
+  ttm <- read_rds(ttm_n)
+  
+  # Assign grid
+  ttm[,grid := current_grid]
+  # Rename origin destination columns
+  setnames(ttm, old = c("origin", "destination"), new = c("from_id", "to_id"))
+  
+  # Join data to ttm
+  # Education data
+  ttm[
+    data_origin, 
+    on = c(grid = "grid", from_id = "id"), 
+    or_edu_decile := or_edu_decile
+  ]
+  # Economic census data
+  ttm[
+    data_destination, 
+    on = c(grid = "grid", to_id = "id"),
+    c("dest_est_ec_un", "dest_est_pe_oc", "dest_inc_decile") :=
+      list(dest_est_ec_un, dest_est_pe_oc, dest_inc_decile)
+  ]
+  
+  # limit parameters for car
+  acc_pars_car <- acc_pars %>% filter(mode == 'car')
+  # Apply access function for different parameters
+  access_all_par <- lapply(1:nrow(acc_pars_car), function(i) {
+    
+    # Estimate accessibility
+    access_origin <- accessibility(
+      ttm = ttm,
+      w = "dest_est_pe_oc",
+      fn = acc_pars_car$dec_fn[i],
+      beta = acc_pars_car$beta[i],
+      mu = acc_pars_car$mu[i],
+      opportunity = acc_pars_car$opportunity[i]
+    )
+    
+    # Assign parameters used for estimates
+    access_origin[, dec_fn := acc_pars_car$dec_fn[i]]
+    access_origin[, beta := acc_pars_car$beta[i]]
+    access_origin[, mu := acc_pars_car$mu[i]]
+    access_origin[, opportunity := acc_pars_car$opportunity[i]]
+    
+    # Return result
+    return(access_origin)
+  })
+  
+  
+  # Bind rows
+  access_all_par <- rbindlist(access_all_par)
+  
+  # Assign grid
+  access_all_par[, grid := current_grid]
+  
+  # Save result by grid
+  write_csv(
+    x = access_all_par, 
+    file = paste0(out, "accessibility_", current_grid, "_car.csv"), 
+    append = FALSE
+  )
+  
+  # Clean env.
+  rm(ttm, access_all_par)
+  gc()
+}
+
+
 
 # Clean env.
 rm(list = ls())
